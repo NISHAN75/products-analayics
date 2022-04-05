148324a22c4599edbff3114922d81c9267db78f0
nishandas <nishandas880@gmail.com>
2022-04-05 13:30:28 +0600

    reviews compelet

--- src/component/DashBoard/DashBoard.js
+++ src/component/DashBoard/DashBoard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashBoard = () => {
   return (
     <div>
-      <h1>this is DashBoard</h1>
+      
     </div>
   );
 };
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+  const [reviews,setReviews]=useReviews();
   return (
-    <div>
-      <h1>this is Reviews</h1>
+    <div className='review-content grid grid-cols-3 gap-10 mt-20 mx-auto text-center'>
+      {
+        reviews.map(review=><Review review={review} key={review.id}></Review>)
+      }
     </div>
   );
 };
